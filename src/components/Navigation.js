d4a40956f8a5703cfa291712ce01a3e3362ace7e
Student_MA118 <Student_MA118@Student-MA118.fios-router.home>
2020-05-15 17:37:29 -0400

    Created image folder. Added a few images to test

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { 
-	Route, 
-	Switch, 
-	Link,
-	BrowserRouter as Router 
-} from 'react-router-dom';
+import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home';
 import Anime from './Anime';
 import Manga from './Manga';
+import '../styles/Navigation.css';
 
 function Navigation() {
 	return (
 		<Router>
-			<nav className="wrapper">
-				<ul className="navi">
+			<nav className="nav-wrapper">
+				<ul className="nav">
 					<li>
 						<Link to="/"> Home </Link>
 					</li>
@@ -36,45 +32,92 @@ function Navigation() {
 
 export default Navigation;
 
-
-
-
 // import React from 'react';
 // // import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 // import Home from './Navigation';
 // import Anime from './Anime';
 // import Manga from './Manga.js';
 // import '../App.css';
-// 
+//
 // function Navigation() {
-	// return (
-		// <Router>
-			{/* <nav className="wrapper"> */}
-				{/* <ul className="nav"> */}
-					{/* <li> */}
-						{/* <Link className="text-white" to="/"> */}
-							{/* Home */}
-						{/* </Link> */}
-					{/* </li> */}
-					{/* <li> */}
-						{/* <Link className="text-white" to="/anime"> */}
-							{/* Anime */}
-						{/* </Link> */}
-					{/* </li> */}
-					{/* <li> */}
-						{/* <Link className="text-white" to="/manga"> */}
-							{/* Manga */}
-						{/* </Link> */}
-					{/* </li> */}
-				{/* </ul> */}
-			{/* </nav> */}
-			{/* <Switch> */}
-				{/* <Route exact path="/" component={Home}></Route> */}
-				{/* {/* <Route exact path="/anime" component={Anime}></Route> */} 
-				{/* {/* <Route exact path="/manga" component={Manga}></Route> */} 
-			{/* </Switch> */}
-		{/* </Router> */}
-	// );
+// return (
+// <Router>
+{
+	/* <nav className="wrapper"> */
+}
+{
+	/* <ul className="nav"> */
+}
+{
+	/* <li> */
+}
+{
+	/* <Link className="text-white" to="/"> */
+}
+{
+	/* Home */
+}
+{
+	/* </Link> */
+}
+{
+	/* </li> */
+}
+{
+	/* <li> */
+}
+{
+	/* <Link className="text-white" to="/anime"> */
+}
+{
+	/* Anime */
+}
+{
+	/* </Link> */
+}
+{
+	/* </li> */
+}
+{
+	/* <li> */
+}
+{
+	/* <Link className="text-white" to="/manga"> */
+}
+{
+	/* Manga */
+}
+{
+	/* </Link> */
+}
+{
+	/* </li> */
+}
+{
+	/* </ul> */
+}
+{
+	/* </nav> */
+}
+{
+	/* <Switch> */
+}
+{
+	/* <Route exact path="/" component={Home}></Route> */
+}
+{
+	/* {/* <Route exact path="/anime" component={Anime}></Route> */
+}
+{
+	/* {/* <Route exact path="/manga" component={Manga}></Route> */
+}
+{
+	/* </Switch> */
+}
+{
+	/* </Router> */
+}
+// );
 // }
 // export default Navigation;
-// 
+//

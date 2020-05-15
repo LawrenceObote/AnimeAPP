e173f18473759cc5aa7aff558ac4b6dc66c2da9f
Student_M17 <student_m17@ps-dal-m17.home>
2020-05-15 15:04:01 -0500

    cleaned up App.js and added SearchAPI component in newly created component folder

---
+++ src/components/SearchAPI.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchAPI extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}

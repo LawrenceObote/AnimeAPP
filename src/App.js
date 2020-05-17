013d6da4658bb5f1e8bc94d53c9cc374a1115743
Lawrence Obote <dev.obote@gmail.com>
2020-05-17 16:45:57 -0400

    cleaned up App.js and created a comment componenet and added firestore/firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+
 import React from 'react';
 import './App.css';
-import CommentExampleComment from './components/Comment';
+import Comment from './components/Comment';
 
 function App() {
   return (
     <div className="App">
-     <CommentExampleComment />
     </div>
   );
 }

ccce7602d12bb360bdd47781f1fb8aa4378c6e95
Lawrence Obote <dev.obote@gmail.com>
2020-05-17 18:14:19 -0400

    added onUpdate button

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,9 +5,19 @@ export const Comment = ({ comment }) => {
   //useState and variable declerations
   const[name, setName] = React.useStat(comment.name);
 
+  const onUpdate = () => {
+    const db = firebase.firestore()
+    //set function
+    db.collection('comments').doc(comment.id).set({...comment, name})
+  }
+
 
   return (
       <div>
+        <input value={name} onChange={e => {
+          setName(e.target.value)}}/>
+          <button onClick={onUpdate}>Update</button>
+        
 
       </div>
   )

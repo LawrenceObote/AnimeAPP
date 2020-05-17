2c253f5155e94209e11b4b9af38de08448355ef1
Lawrence Obote <dev.obote@gmail.com>
2020-05-17 17:54:45 -0400

    added button and input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Comment from './components/Comment';
 function App() {
   return (
     <div className="App">
+      <input value={newCommentName}
+             onChange={e => setNewCommentName(e.target.value)}
+      />
+      <button onClick={onCreate}>Create</button>
+      {comments.map(comment => (
+        <li key={comment.name}>{comment.name}
+        <Comment comment={comment}/>
+        </li> 
+      ))}
     </div>
   );
 }

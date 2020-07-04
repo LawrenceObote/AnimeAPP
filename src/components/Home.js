3f99f13c8a237cb50ddf3fa11d5f663853182b40
LawrenceObote <lawrencerobote@gmail.com>
2020-07-03 23:34:38 -0400

    Changed submit buttons and some spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,173 +6,152 @@ import Pika from '../images/Pika.png';
 import umaru from '../images/umaru.png';
 
 class Home extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			info: [],
-			info2: [],
-			search: false,
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+            info2: [],
+            search: false,
+        };
 
-		this.clearButton = this.clearButton.bind(this);
-		this.submitButton = this.submitButton.bind(this);
-		this.getInfo = this.getInfo.bind(this);
-		this.randomCat = this.randomCat.bind(this);
-	}
+        this.clearButton = this.clearButton.bind(this);
+        this.submitButton = this.submitButton.bind(this);
+        this.getInfo = this.getInfo.bind(this);
+        this.randomCat = this.randomCat.bind(this);
+    }
 
-	async randomCat() {
-		try {
-			const [getAnimeData, getData] = await Promise.all([
-				axios.get(
-					`https://kitsu.io/api/edge/anime?filter[categories]=${this.state.answers}`
-				),
-				axios.get(
-					`https://kitsu.io/api/edge/manga?filter[categories]=${this.state.answers}`
-				),
-			]);
+    async randomCat() {
+        try {
+            const [getAnimeData, getData] = await Promise.all([
+                axios.get(
+                    `https://kitsu.io/api/edge/anime?filter[categories]=${this.state.answers}`
+                ),
+                axios.get(
+                    `https://kitsu.io/api/edge/manga?filter[categories]=${this.state.answers}`
+                ),
+            ]);
 
-			// const getData = await axios.get(
-			// `https://kitsu.io/api/edge/manga?filter[categories]=${this.state.answers}`
-			// );
+            // const getData = await axios.get(
+            // `https://kitsu.io/api/edge/manga?filter[categories]=${this.state.answers}`
+            // );
 
-			this.setState({
-				info: getAnimeData.data.data,
+            this.setState({
+                info: getAnimeData.data.data,
 
-				info2: getData.data.data,
-			});
+                info2: getData.data.data,
+            });
 
-			console.log(this.state.info);
-			console.log(getData);
-			console.log(getAnimeData);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+            console.log(this.state.info);
+            console.log(getData);
+            console.log(getAnimeData);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-	getInfo(event) {
-		event.preventDefault();
-		this.setState({ answers: event.target.value });
-	}
+    getInfo(event) {
+        event.preventDefault();
+        this.setState({ answers: event.target.value });
+    }
 
-	submitButton(e) {
-		e.preventDefault();
-		this.setState({ search: true });
-	}
+    submitButton(e) {
+        e.preventDefault();
+        this.setState({ search: true });
+    }
 
-	clearButton() {
-		this.setState({ search: false, answers: '' });
-	}
+    clearButton() {
+        this.setState({ search: false, answers: '' });
+    }
 
-	render() {
-		return (
-			<div>
-				<div className="home-wrapper">
-					<form className="home-form" onSubmit={this.submitButton}>
-						<br />
-						<input
-							className="home-text-field"
-							id="borderimg1"
-							type="text"
-							value={this.state.answers}
-							onChange={this.getInfo}
-						/>
-						<br />
-						<h3 className="text-white search">
-							{' '}
-							Anime & Manga search by Category{' '}
-						</h3>
+    render() {
+        return (
+            <div>
+                <div className="home-wrapper">
+                    <form onSubmit={this.submitButton}>
+                        <input
+                            id="search-anime"
+                            type="text"
+                            value={this.state.answers}
+                            onChange={this.getInfo}
+                        />
+                        <h3 className="text-white">Anime and Manga Search by Category</h3>
+                        <h3 className="text-white">i.e. "horror"</h3>
+                        <input className="button" type="submit" value="Submit" onClick={this.randomCat} />
+                    </form>
+                </div>
+                <br />
 
-						<br />
-						<input
-							className="text-button"
-							id="home-sumbit"
-							type="image"
-							src={umaru}
-							alt="Umaru with a coke"
-							onClick={this.randomCat}
-						/>
-						<input
-							className="text-button"
-							id="home-clear"
-							type="image"
-							src={Pika}
-							alt="Pikachu"
-							onClick={this.clearButton}
-						/>
-					</form>
-				</div>
-
-				<div>
-					{this.state.info.map((response, index) => {
-						return (
-							<div className="home-response" key={index}>
-								<h1 className="text-body">
-									Title : {response.attributes.canonicalTitle} <br />
+                <div>
+                    {this.state.info.map((response, index) => {
+                        return (
+                            <div className="home-response" key={index}>
+                                <h1 className="text-body">
+                                    Title : {response.attributes.canonicalTitle} <br />
 									Japenese Title : {response.attributes.titles.ja_jp}
-								</h1>
-								<br></br>
-								<img
-									className="anime-cover"
-									src={response.attributes.posterImage.small}
-									alt="Anime-Poster-Cover"
-								></img>
-								<br />
+                                </h1>
+                                <br></br>
+                                <img
+                                    className="anime-cover"
+                                    src={response.attributes.posterImage.small}
+                                    alt="Anime-Poster-Cover"
+                                ></img>
+                                <br />
 
-								<p>
-									Series Status : {response.attributes.status}
-									<br />
+                                <p>
+                                    Series Status : {response.attributes.status}
+                                    <br />
 									Start of Series : {response.attributes.startDate}
-									<br />
+                                    <br />
 									End of Series : {response.attributes.endDate}
-									<br />
+                                    <br />
 									Type : {response.type}
-								</p>
+                                </p>
 
-								<p>
-									Rating : {response.attributes.ageRating}
-									<br />
+                                <p>
+                                    Rating : {response.attributes.ageRating}
+                                    <br />
 									Rating Guide : {response.attributes.ageRatingGuide}
-									<br />
+                                    <br />
 									Episodes Count : {response.attributes.episodeCount}
-								</p>
-								<p> {response.attributes.synopsis} </p>
-							</div>
-						);
-					})}
-				</div>
+                                </p>
+                                <p> {response.attributes.synopsis} </p>
+                            </div>
+                        );
+                    })}
+                </div>
 
-				<div>
-					{this.state.info2.map((response, index) => {
-						return (
-							<div className="home-response" key={index}>
-								<img
-									className="manga-cover"
-									src={response.attributes.posterImage.small}
-									alt="Manga-Poster-Cover"
-								></img>
-								<br />
-								<h4 className="text-body">
-									Title : {response.attributes.canonicalTitle} <br />
+                <div>
+                    {this.state.info2.map((response, index) => {
+                        return (
+                            <div className="home-response" key={index}>
+                                <img
+                                    className="manga-cover"
+                                    src={response.attributes.posterImage.small}
+                                    alt="Manga-Poster-Cover"
+                                ></img>
+                                <br />
+                                <h4 className="text-body">
+                                    Title : {response.attributes.canonicalTitle} <br />
 									Japenese Title : {response.attributes.titles.ja_jp}
-								</h4>
-								<p>
-									Rating : {response.attributes.ageRating}
-									<br />
-									<br />
+                                </h4>
+                                <p>
+                                    Rating : {response.attributes.ageRating}
+                                    <br />
+                                    <br />
 									Type : {response.type}
-								</p>
-								<p>
-									Series Status : {response.attributes.status}
-									<br />
+                                </p>
+                                <p>
+                                    Series Status : {response.attributes.status}
+                                    <br />
 									Chapter Count : {response.attributes.chapterCount}
-								</p>
-								<p> {response.attributes.synopsis} </p>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+                                </p>
+                                <p> {response.attributes.synopsis} </p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
 export default Home;
--- src/components/Manga.js
+++ src/components/Manga.js
@@ -5,127 +5,112 @@ import Aggretsuko from '../images/Aggretsuko.png';
 import '../styles/MangaPage.css';
 
 class Manga extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			info: [],
-			search: false,
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+            search: false,
+        };
 
-		this.clearButton = this.clearButton.bind(this);
-		this.submitButton = this.submitButton.bind(this);
-		this.getInfo = this.getInfo.bind(this);
-		this.getManga = this.getManga.bind(this);
-	}
+        this.clearButton = this.clearButton.bind(this);
+        this.submitButton = this.submitButton.bind(this);
+        this.getInfo = this.getInfo.bind(this);
+        this.getManga = this.getManga.bind(this);
+    }
 
-	async getManga() {
-		try {
-			const getManga = await axios.get(
-				`https://kitsu.io/api/edge/manga?filter[text]=${this.state.answers}`
-			);
+    async getManga() {
+        try {
+            const getManga = await axios.get(
+                `https://kitsu.io/api/edge/manga?filter[text]=${this.state.answers}`
+            );
 
-			this.setState({
-				info: getManga.data.data,
-			});
-			console.log(this.state.info);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+            this.setState({
+                info: getManga.data.data,
+            });
+            console.log(this.state.info);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-	getInfo(event) {
-		event.preventDefault();
-		this.setState({ answers: event.target.value });
-	}
+    getInfo(event) {
+        event.preventDefault();
+        this.setState({ answers: event.target.value });
+    }
 
-	submitButton(e) {
-		e.preventDefault();
-		this.setState({ search: true });
-	}
+    submitButton(e) {
+        e.preventDefault();
+        this.setState({ search: true });
+    }
 
-	clearButton() {
-		this.setState({ answers: '', search: false });
-	}
+    clearButton() {
+        this.setState({ answers: '', search: false });
+    }
 
-	render() {
-		return (
-			<div>
-				<div className="manga-wrapper">
-					<form className="manga-form" onSubmit={this.submitButton}>
-						<br />
-						<input
-							className="manga-text-field"
-							id="borderimg1"
-							type="text"
-							value={this.state.answers}
-							onChange={this.getInfo}
-						/>
-						<h3 className="text-white">Manga Search</h3>
+    render() {
+        return (
+            <div>
+                <div className="manga-wrapper">
 
-						<br />
-						<input
-							className="text-button"
-							id="manga-sumbit"
-							type="image"
-							src={Natsu}
-							alt="Natsu from Fairy Tails"
-							onClick={this.getManga}
-						/>
-						<input
-							className="text-button1"
-							id="manga-clear"
-							type="image"
-							src={Aggretsuko}
-							alt="Aggretsuko"
-							onClick={this.clearButton}
-						/>
-					</form>
-				</div>
+                    <form onSubmit={this.submitButton}>
+                        <input
+                            id="search-anime"
+                            type="text"
+                            value={this.state.answers}
+                            onChange={this.getInfo}
+                        />
+                        <h3 className="text-white">Manga Search</h3>
+                        <input className="button" type="submit" value="Submit" onClick={this.getManga} />
+                    </form>
+                </div>
+                <br />
 
-				<div>
-					{this.state.info.map((response, index) => {
-						return (
-							<div className="manga-response" key={index}>
-								<h1 className="text-white text-b">
-									Title : {response.attributes.canonicalTitle}
-								</h1>
-								<h1 className="text-white text-b">
-									Japenese Title : {response.attributes.titles.ja_jp}
-								</h1>
-								<br></br>
-								<img
-									className="manga-cover"
-									src={response.attributes.posterImage.small}
-									alt="Manga-Poster-Cover"
-								></img>
-								<br />
-								<p> Rated : {response.attributes.ageRating} 
-								<br />
-								Rating : {response.attributes.averageRating}
-								</p>
-								<br></br>
-								<br></br>
-								<p>
-									Series Status : {response.attributes.status}
-									<br></br>
-									Start Date : {response.attributes.startDate}
-									<br></br>
-									End Date : {response.attributes.endDate}
-								</p>
-								<br></br>
-								<h4 className="text-body">
-									Volume Count : {response.attributes.volumeCount} <br />
-									Serialization : {response.attributes.serialization}
-								</h4>
-								<br></br>
-								<p> {response.attributes.synopsis} </p>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+                <div>
+                    {this.state.info.map((response, index) => {
+                        return (
+                            <div className="manga-response" key={index}>
+                                <h1 className="text-white text-b">
+                                    Title : {response.attributes.canonicalTitle}
+                                </h1>
+                                <h1 className="text-white text-b">
+                                    Japenese Title : {response.attributes.titles.ja_jp}
+                                </h1>
+                                <br></br>
+                                <img
+                                    className="manga-cover"
+                                    src={response.attributes.posterImage.small}
+                                    alt="Manga-Poster-Cover"
+                                ></img>
+                                <br />
+                                <p>
+                                    {' '}
+                  Rated : {response.attributes.ageRating}
+                                    <br />
+                  Rating : {response.attributes.averageRating}
+                                </p>
+                                <br></br>
+                                <br></br>
+                                <p>
+                                    Series Status : {response.attributes.status}
+                                    <br></br>
+                  Start Date : {response.attributes.startDate}
+                                    <br></br>
+                  End Date : {response.attributes.endDate}
+                                </p>
+                                <br></br>
+                                <h4 className="text-body">
+                                    Volume Count : {response.attributes.volumeCount} <br />
+                  Serialization : {response.attributes.serialization}
+                                </h4>
+                                <br></br>
+                                <p> {response.attributes.synopsis} </p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Manga;
